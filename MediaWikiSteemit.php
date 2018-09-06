904743d30157acaae9412dba9a777182c566aaaf
Michael Ten <scott71@users.noreply.github.com>
2018-09-05 20:50:48 -0700

    Create MediaWikiSteemit.php
    Creating a MediaWiki extension that enable the ability to check a box to publish wiki pages on Steemit under a Creative Commons license. Wiki formatting and links should automatically be reformatted for Steemit.

---
+++ MediaWikiSteemit.php
@@ -0,0 +1,5 @@
+
+/* Creating a MediaWiki extension that enable the ability to check a box to publish */
+/* wiki pages on Steemit under a Creative Commons license. Wiki formatting and links */
+/* should automatically be reformatted for Steemit. */ 
+
